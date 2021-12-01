76a9354f4d06893c32c8ea39b7f8905b82875020
Jay Harris <jaywritescode@users.noreply.github.com>
2021-12-01 13:21:19 -0500

    problem 1, part 2

--- sonar_sweep.py
+++ sonar_sweep.py
@@ -1,11 +1,16 @@
-#%%
+from more_itertools import triplewise
+
 def count_increases(depths):
     return sum(1 for x in zip(depths, depths[1:]) if x[0] < x[1])
-#%%
 
+def count_increases_sliding(depths):
+    counter = 0
+    for (m, n) in zip(triplewise(depths), triplewise(depths[1:])):
+        if sum(m) < sum(n):
+            counter += 1
+    return counter
 
 if __name__ == '__main__':
     with open("input-01.txt") as f:
         args = [int(x) for x in f.readlines()]
-        print(count_increases(args))
-# %%
+        print(count_increases_sliding(args))
