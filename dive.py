560d1906671ebe8252ed950cb7ff194975706f59
Jay Harris <jharris@squarespace.com>
2021-12-02 14:51:30 -0500

    problem 2, part 1

---
+++ dive.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+
+Command = namedtuple('Command', ['direction', 'amount'])
+
+class Dive:
+    def __init__(self):
+        self.depth = 0
+        self.horizontal = 0
+
+    def run_command(self, command):
+        if command.direction == 'forward':
+            self.horizontal += command.amount
+        else:
+            self.depth += command.amount if command.direction == 'down' else -(command.amount)
+
+    def solve(self, course):
+        for cmd in [Dive.parse_command(line) for line in course]:
+            self.run_command(cmd)
+        return self.depth * self.horizontal
+
+    @staticmethod
+    def parse_command(line):
+        direction, amount = line.split()
+        return Command(direction, int(amount))
+
+
+if __name__ == '__main__':
+    with open('input-02.txt') as file:
+        commands = file.readlines()
+
+        d = Dive().solve(commands)
+        print(d)
+
