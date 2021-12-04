dbdb47fd156181f54ff61efb872f8db1c61295a4
Jay Harris <jaywritescode@users.noreply.github.com>
2021-12-03 22:57:36 -0500

    problem 1, part 2 in lisp

--- sonar-sweep.lisp
+++ sonar-sweep.lisp
@@ -1,2 +1,17 @@
-(defun sonar-sweep-part-one (input)
+;;; part one
+(defun sonar-sweep (input)
   (count t (map 'list #'< input (rest input))))
+
+;;; part two
+(defun triples (seq)
+  (map 'list #'list seq (cdr seq) (cddr seq)))
+
+(defun sonar-sweep-2 (input)
+  ;; Is there a simpler way to write this mapcar?
+  (let ((window-sums (mapcar #'(lambda (tuple) (reduce #'+ tuple))
+                             (triples problem-input))))
+    (count t (map 'list #'< window-sums (rest window-sums)))))
+
+;;; good to remember
+(defvar problem-input
+  (mapcar #'parse-integer (uiop:read-file-lines "input-01.txt")))
