3b059fd90dfa986440e1462d27a5334d41c30380
Jay Harris <jharris@squarespace.com>
2021-12-06 15:21:00 -0500

    problem 6, part 2

--- lanternfish.py
+++ lanternfish.py
@@ -1,5 +1,5 @@
 #%%
-def lanternfish(start):
+def lanternfish_naive(start):
     state = start[:]
     for _ in range(80):
         spawned = 0
@@ -13,7 +13,24 @@ def lanternfish(start):
     return len(state)
 #%%
 
+from collections import Counter
+
+def lanternfish(start):
+    counter = Counter(start)
+    for _ in range(256):
+        updated = dict()
+        
+        updated[8] = counter[0]
+        updated[7] = counter[8]
+        updated[6] = counter[7] + counter[0]
+        for i in range(5, -1, -1):
+            updated[i] = counter[i + 1]
+
+        counter = Counter(updated)
+    return sum(counter.values())
+
 if __name__ == '__main__':
     with open('input-06.txt') as file:
         puzzle = [int(x) for x in file.readline().split(',')]
         print(lanternfish(puzzle))
+# %%
