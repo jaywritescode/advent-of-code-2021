df15a419f2142f0e75ec97f059d3515de5400242
Jay Harris <jaywritescode@users.noreply.github.com>
2021-12-08 11:38:09 -0500

    problem 8, part 1

---
+++ seven_segment_search.py
@@ -0,0 +1,24 @@
+def simple_digits_in_line(line):
+    output_value = line.split('|')[1].split()
+    return len(list(filter(lambda v: len(v) in [2,3,4,7], output_value)))
+
+def count_simple_digits(puzzle):
+    return sum(simple_digits_in_line(line) for line in puzzle)
+
+
+test = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce""".splitlines()
+
+
+if __name__ == '__main__':
+    with open('input-08.txt') as file:
+        puzzle = [line.strip() for line in file.readlines()]
+        print(count_simple_digits(puzzle))
