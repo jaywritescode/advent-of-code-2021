c6f4c0d2722b587dd8cb76b7b0995e10bb81df81
Jay Harris <jharris@squarespace.com>
2021-12-13 11:37:40 -0500

    problem 12, easier debugging time

--- passage_pathing.py
+++ passage_pathing.py
@@ -1,21 +1,21 @@
 from collections import deque, namedtuple
 
 
-class Cave(namedtuple('Cave', ['name', 'neighbors'])):
+class Cave:
+    def __init__(self, name):
+        self.name = name
+        self.neighbors = set()
+
     def __hash__(self):
         return hash(self.name)
 
     def __repr__(self):
-        return f"Cave {self.name} -> {[x.name for x in self.neighbors]}"
-
-    @classmethod
-    def create(cls, name):
-        return Cave(name=name, neighbors=set())
+        return f"Cave {self.name}: with neighbors {[x.name for x in self.neighbors]}"
 
 
 class PassagePathing:
     def __init__(self, edges):
-        self.start = Cave.create('start')
+        self.start = Cave('start')
         self.graph = dict(start=self.start)
         for edge in edges:
             v1, v2 = edge.strip().split('-')
@@ -23,9 +23,9 @@ class PassagePathing:
 
     def add_edge(self, v1, v2):
         if v1 not in self.graph:
-            self.graph[v1] = Cave.create(v1)
+            self.graph[v1] = Cave(v1)
         if v2 not in self.graph:
-            self.graph[v2] = Cave.create(v2)
+            self.graph[v2] = Cave(v2)
 
         cave1, cave2 = self.graph[v1], self.graph[v2]
         cave1.neighbors.add(cave2)
@@ -34,9 +34,9 @@ class PassagePathing:
     def solve(self):
         paths = []
 
-        queue = deque([[self.start]])
-        while queue:
-            e = queue.popleft()  # e is a path
+        paths_to_expolore = deque([[self.start]])
+        while paths_to_expolore:
+            e = paths_to_expolore.popleft()
             current = e[-1]
 
             for n in current.neighbors:
@@ -44,13 +44,13 @@ class PassagePathing:
                     # don't revisit a small cave
                     continue
                 
-                updated_path = list(current[:])
+                updated_path = list(e[:])
                 updated_path.append(n)
-                if n == 'end':
+                if n.name == 'end':
                     paths.append(updated_path)
                     continue
 
-                queue.append(updated_path)
+                paths_to_expolore.append(updated_path)
 
         return len(paths)
 
@@ -63,7 +63,18 @@ b-d
 A-end
 b-end""".splitlines()
 
+test2 = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc""".splitlines()
+
 
 if __name__ == '__main__':
-    p = PassagePathing(test1)
+    p = PassagePathing(test2)
     print(p.solve())
