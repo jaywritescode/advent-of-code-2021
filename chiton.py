b7d1fc93c11e4902154b11462b7f7e99372b7977
Jay Harris <jaywritescode@users.noreply.github.com>
2021-12-21 23:49:58 -0500

    refactor

--- chiton.py
+++ chiton.py
@@ -2,45 +2,43 @@ from collections import namedtuple
 
 Coordinate = namedtuple('Coordinate', ['row', 'column'])
 
-class Node:
-    def __init__(self, name, value, graph):
-        self.name = name
-        self.value = value
-        self.graph = graph
-
-        self.min_distance = float('inf')
-        self.neighbors = None
-        self.previous = None
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __repr__(self):
-        return f"{self.name}: value = {self.value}, min_distance = {self.min_distance}"
-
 class Chiton:
-    def __init__(self, root, nodes):
-        self.root = root
-        self.nodes = nodes
+
+    class Node:
+        def __init__(self, name, value):
+            self.value = value
+            self.min_distance = float('inf')
+            self.neighbors = None
+            self.name = name
+
+        def __hash__(self):
+            return hash(self.name)
+
+        def __repr__(self):
+            return f"{self.name}: value = {self.value}, min_distance = {self.min_distance}"
+
+    def __init__(self):
+        self.nodes = dict()
+
+    def create_node(self, name, value):
+        self.nodes[name] = Chiton.Node(name, value)
 
     def get_neighbors(self, node):
-        if node.graph is not self:
-            raise
-
-        row, column = node.name
         if node.neighbors is None:
             node.neighbors = set()
-            for delta_row, delta_column in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                possible_neighbor_coordinate = Coordinate(row + delta_row, column + delta_column)
-                if possible_neighbor_coordinate in self.nodes:
-                    node.neighbors.add(self.nodes[possible_neighbor_coordinate])
+            for (delta_r, delta_c) in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                neighbor = self.nodes.get(Coordinate(node.name.row + delta_r, node.name.column + delta_c))
+                if neighbor:
+                    node.neighbors.add(neighbor)
+
         return node.neighbors
 
     def dijkstra(self, end_node):
-        self.root.min_distance = 0
+        root = self.nodes[Coordinate(0, 0)]
+        root.min_distance = 0
 
         visited = set()
-        fringe = { self.root }
+        fringe = { root }
         
         while fringe:
             current_node = min(fringe, key=lambda node: node.min_distance)
@@ -50,7 +48,7 @@ class Chiton:
             if current_node == end_node:
                 return current_node
 
-            for neighbor in current_node.neighbors:
+            for neighbor in self.get_neighbors(current_node):
                 if neighbor in visited:
                     continue
                 fringe.add(neighbor)
@@ -62,25 +60,22 @@ class Chiton:
         # no solution
         return None
 
-    def solve(self, end_node):
-        e = self.dijkstra(end_node)
-        return e.min_distance    
+    def solve(self):
+        max_row = max(c.row for c in self.nodes)
+        max_col = max(c.column for c in self.nodes)
+
+        e = self.dijkstra(self.nodes[Coordinate(max_row, max_col)])
+        return e.min_distance
 
 
 if __name__ == '__main__':
-    nodes = dict()
-    with open('input-15.txt') as file:
+    chiton = Chiton()
+
+    with open('sample-data/sample-15.txt') as file:
         for row, line in enumerate(file):
             for column, value in enumerate(iter(line.strip())):
-                this_node = Node((row, column), int(value))
-                nodes[(row, column)] = this_node
-                if row > 0:
-                    nodes[(row - 1, column)].neighbors.add(this_node)
-                    this_node.neighbors.add(nodes[(row - 1, column)])
-                if column > 0:
-                    nodes[(row, column - 1)].neighbors.add(this_node)
-                    this_node.neighbors.add(nodes[(row, column - 1)])
+                chiton.create_node(Coordinate(row, column), int(value))
 
-    puzzle = Chiton(nodes[(0, 0)])
-    print(puzzle.solve(this_node))
+        print(chiton.solve())
+
 
