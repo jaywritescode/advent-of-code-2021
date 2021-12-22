8e824fde804341d0fa3410711fd456aaa7caaec4
Jay Harris <jharris@squarespace.com>
2021-12-22 13:25:56 -0500

    problem 16, part 1

--- package_decoder.py
+++ package_decoder.py
@@ -1,42 +1,46 @@
 #%%
+from collections import namedtuple
 from more_itertools import take
-from more_itertools.more import chunked
+from more_itertools.more import chunked, peekable, spy
 
-def hex2bin(bits):
-    return bin(int(bits, base=16))[2:]
+
+Packet = namedtuple('Packet', ['version', 'type_id', 'payload'])
+
+def hex2bin(bits: str) -> str:
+    repr = bin(int(bits, base=16))[2:]
+    while len(repr) % 4 != 0:
+        repr = '0' + repr
+    return repr
 
 def bits2dec(iterable):
-    print(f"bits2dec: {iterable}")
     return int(''.join(iterable), base=2)
 
+
+def to_packets(hex):
+    b = hex2bin(hex)
+    i = iter(b)
+    return parse_next_packet(i)
+
 def parse_next_packet(bitstream):
-    i = iter(bitstream)
-    version = bits2dec(take(3, i))
-    type_id = bits2dec(take(3, i))
+    version = bits2dec(take(3, bitstream))
+    type_id = bits2dec(take(3, bitstream))
 
     if type_id == 4:
-        payload = parse_literal_value(i)
+        payload = parse_literal_value(bitstream)
     else:
-        payload = parse_operator_packet(i)
+        payload = parse_operator_packet(bitstream)
 
-    print(version)
-    print(type_id)
-    print(payload)
-#%%
+    return Packet(version, type_id, payload)    
+
 def parse_literal_value(bitstream):
     binary_value = []
     chunks = chunked(bitstream, 5)
     while True:
         chunk = next(chunks)
-        print(f"chunk: {chunk}")
         is_last, payload = chunk[0], chunk[1:]
         binary_value.extend(payload)
         if is_last == '0':
             return bits2dec(binary_value)
-
-
-
-# %%
 
 def parse_operator_packet(bitstream):
     if next(bitstream) == '0':
@@ -46,16 +50,33 @@ def parse_operator_packet(bitstream):
 
 def parse_subpackets_given_length(bitstream):
     length = bits2dec(take(15, bitstream))
-    subbitstream = take(length, bitstream)
+    p = peekable(take(length, bitstream))
     
     subpackets = []
-    while subbitstream:
-        subpackets.append(parse_next_packet(subbitstream))
+    while p.peek(None):
+        subpackets.append(parse_next_packet(p))
     return subpackets
-
+        
 def parse_subpackets_given_count(bitstream):
     count = bits2dec(take(11, bitstream))
+    
     subpackets = []
     for _ in range(count):
         subpackets.append(parse_next_packet(bitstream))
     return subpackets
+
+def version_sum(root_packet):
+    n = root_packet.version
+    s = 0 if root_packet.type_id == 4 else sum(version_sum(p) for p in root_packet.payload)
+
+    return n + s
+    
+def solve(puzzle):
+    packet = to_packets(puzzle)
+    return version_sum(packet)
+
+
+
+if __name__ == '__main__':
+    with open('input-16.txt') as file:
+        print(solve(file.readline()))
