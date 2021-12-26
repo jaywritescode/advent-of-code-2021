759292dc62cd42721b8b4441a488d34f2ddf183f
Jay Harris <jaywritescode@users.noreply.github.com>
2021-12-26 14:43:50 -0500

    problem 2, part 2 in lisp

--- dive.lisp
+++ dive.lisp
@@ -1,4 +1,4 @@
-(defun solve (puzzle-input)
+(defun solve-part-one (puzzle-input)
   (let ((horizontal-pos 0)
         (depth 0))
     (dolist (command puzzle-input (* horizontal-pos depth))
@@ -8,10 +8,25 @@
           (|up| (decf depth magnitude))
           (|down| (incf depth magnitude)))))))
 
+(defun solve-part-two (puzzle-input)
+  (let ((horizontal-pos 0)
+        (depth 0)
+        (aim 0))
+    (dolist (command puzzle-input (* horizontal-pos depth))
+      (let ((magnitude (alexandria:assoc-value command :magnitude)))
+        (case (alexandria:assoc-value command :direction)
+          (|forward| (setf horizontal-pos (+ horizontal-pos magnitude)
+                           depth (+ depth (* aim magnitude))))
+          (|up| (decf aim magnitude))
+          (|down| (incf aim magnitude)))))))
+
 (defun parse-line (line)
   (let ((command (str:words line)))
     (pairlis '(:direction :magnitude)
              (list (intern (first command)) (parse-integer (second command))))))
 
-(defun dive (filename)
-  (solve (mapcar #'parse-line (uiop:read-file-lines filename))))
+(defun dive (filename &optional solve-part-one)
+  (let ((puzzle-input (mapcar #'parse-line (uiop:read-file-lines filename))))
+    (if solve-part-one
+        (solve-part-one puzzle-input)
+        (solve-part-two puzzle-input))))
